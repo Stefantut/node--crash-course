1aa96641d19e58c84f30a9799b63418177107cfe
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-09-27 23:16:49 +0100

    Read a file with fs

--- fs.js
+++ fs.js
@@ -3,8 +3,14 @@ const fs = require("fs");
 
 //async function - the event loop
 // takes 3 arguments (the name of the file, the value, a callback function)
-fs.writeFile("message.txt", "Hello there", (err) => {
-  if (err) throw err;
+// fs.writeFile("message.txt", "Hello there", (err) => {
+//   if (err) throw err;
 
-  console.log("file has been written");
+//   console.log("file has been written");
+// });
+
+// to read a file
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
